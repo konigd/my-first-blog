cb7403da97d594b7dedc77fb1eb876e3d096f35a
Daniela Puchkov <soleil.mdsv@gmail.com>
2017-03-12 17:31:07 +0100

    thank you for submitting

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
 
 # Create your views here.
